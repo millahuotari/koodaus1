33780e7dcf09fe629717bdad09c3d5cf1f492288
Milla Huotari <48395@edu.raseko.fi>
2023-01-23 14:59:21 +0200

    lisää juttuja

--- harjoitus2b.py
+++ harjoitus2b.py
@@ -3,5 +3,9 @@ import harjoitus2 as h2
 
 if __name__ == "__main__":
     print("Kysytään 2 nimeä:")
+    joku_juttu = 1
     nimet = h2.kysy_nimet(2)
+    joku_juttu = 2
+    nimet.append("Ekstra nimi")
+    joku_juttu = 3
     print("Nimet ovat", nimet)
