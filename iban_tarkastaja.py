7a27e3221908d01fbc452ea2bc0018bb0b084207
Milla Huotari <48395@edu.raseko.fi>
2023-01-30 13:16:59 +0200

    Muotoile

--- iban_tarkastaja.py
+++ iban_tarkastaja.py
@@ -5,6 +5,8 @@ def main():
     on_oikea_iban = iban.is_valid(tilinumero)
     if on_oikea_iban:
         print(f"Antamasi tilinumero '{tilinumero}' on oikea IBAN. ")
+        muotoiltu = iban.format(tilinumero)
+        print(f"Tilinumero muotoiltuna: {muotoiltu}")
     else:
         print(f"Antamasi tilinumero '{tilinumero}' ei ole IBAN")
 
